e5d09b8fe7ce685a0e33673afb508dc3c600c643
Muhammad Abdullah <muhammadabdullah284@gmail.com>
2020-02-15 16:29:43 +0500

    updated justforfun

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const dep = require("./my-dependency.js");
-module.exports = (req, res) => {
+const fs = require("fs").promises;
+
+module.exports = async (req, res) => {
   dep(`Request Body: ${req.body}`);
-  res.send("Request body printed.");
+  const file = await fs.readFile("./index.js");
+  console.log(file.toString("utf-8"));
+  res.json({message: "Here is the file.", file: file.toString("utf-8")});
+
 }
